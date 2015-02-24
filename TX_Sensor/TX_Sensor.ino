826e4508ae7060d163aa888675ac72cb038a9e22
Tahsin Rahman <madhacker3kxl@hotmail.com>
2015-02-24 17:53:49 -0600

    Added pin change interrupt and setup watchdog

--- TX_Sensor/TX_Sensor.ino
+++ TX_Sensor/TX_Sensor.ino
@@ -8,11 +8,17 @@
 #define TX   PINB4      //TX Pin
 #define LED  PINB0		//LED Pin
 #define Sens PINB2      //Input pin
+
 #define low_Batt_code    101010 //Code for low battery
+#define Open_D           8767848
+#define Close_D          8767847
 
 RCSwitch mySwitch = RCSwitch();
 
-boolean lowBattery = false; //used to check if the interrupt has raised low battery
+volatile boolean lowBattery = false; //Used to check if the interrupt has raised low battery
+volatile boolean is_open = false;    //If it is true that means the relay is open
+volatile boolean wdt_setup = false;
+volatile boolean wdt_on = false;
 
 void setup() {
     //Power off not needed stuff
@@ -28,25 +34,37 @@ void setup() {
     DDRB  |= (1 << LED);	//Set notification pin as output
     PORTB &= ~(1 << LED);   //Set the notification pin as low just in case
 
-    DDRB  &= (1<< Sens);    //Set Sens pin as input
+    DDRB  &= ~(1<< Sens);    //Set Sens pin as input
 
     mySwitch.enableTransmit(TX);    //Enable transmit on pin TX
     mySwitch.setRepeatTransmit(2);  //Send the code twice just in case
 
+    attachInterrupt(INT0, wakeup, LOW);
+
     //Notify the user mcu has loaded fine
     beep();
     beep();
+}
 
 void loop() {
-    analogComparator.setOn(INTERNAL_REFERENCE, AIN1); //Setup analog comparator
-    analogComparator.enableInterrupt(battStatus, LOW);
-
-    if (lowBattery) {
-        mySwitch.send(low_Batt_code, 24); //send the 24bit low battery code
+    if (wdt_setup) {
+        send_data(Open_D); //Send data out for open contact
+        setup_watchdog(8); //Turn on watchdog for 4 sec delay
+    }
+    if (wdt_on)
+    {
+        PORTB |= (1 << LED);
     }
 
-    else sleep();
+    else
+    {
+        wdt_disable(); //Turn off the WDT!
+        attachInterrupt(INT0, wakeup, LOW);
+    }
+
+    sleep();
 }
+
 
 void sleep(){
     set_sleep_mode(SLEEP_MODE_PWR_DOWN); //Set sleep mode to deep sleep
@@ -59,13 +77,54 @@ void sleep(){
 }
 
 void beep(){
+    LED_on();
     delay(500);
-    PORTB |= (1 << LED);     //Turn on
+    LED_off();
     delay(500);
-    PORTB &= ~(1 << LED);    //Turn off
+}
+
+void check_battery(){
+    analogComparator.setOn(INTERNAL_REFERENCE, AIN1); //Setup analog comparator
+    analogComparator.enableInterrupt(battStatus, LOW);
+    if (lowBattery) {
+        LED_on();                         //Turn on LED
+        mySwitch.send(low_Batt_code, 24); //send the 24bit low battery code
+    }
+}
+
+void setup_watchdog(int timerPrescaler) {
+    if (timerPrescaler > 9 ) timerPrescaler = 9; //Limit incoming amount to legal settings
+    byte bb = timerPrescaler & 7;
+    if (timerPrescaler > 7) bb |= (1<<5);        //Set the special 5th bit if necessary
+
+    //This order of commands is important and cannot be combined
+    MCUSR &= ~(1<<WDRF);                         //Clear the watch dog reset
+//    WDTCR |= (1<<WDCE) | (1<<WDE);               //Set WD_change enable, set WD enable
+//    WDTCR = bb;                                  //Set new watchdog timeout value
+ //   WDTCR |= _BV(WDIE);                          //Set the interrupt enable, this will keep unit from resetting after each int
 }
 
 //interrupt to be raised by the analog comparator
-void battStatus() {
+void battStatus(){
     lowBattery = true; //let's inform the main loop that the condition has been reached by the analog comparator
+}
+
+//interrupt for contact open
+void wakeup(){
+        wdt_setup = true;
+        detachInterrupt(INT0);
+}
+
+void send_data(int data){
+         LED_on();
+         mySwitch.send(data, 24);
+         LED_off();
+}
+
+void LED_on(){
+    PORTB |= (1 << LED);     //Turn on LED
+}
+
+void LED_off(){
+    PORTB &= ~(1 << LED);    //Turn off LED
 }
--- TX_Sensor/Visual Micro/.TX_Sensor.vsarduino.h
+++ TX_Sensor/Visual Micro/.TX_Sensor.vsarduino.h
@@ -5,23 +5,28 @@
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: ATtiny x5 w/ ATtiny85 @ 1 MHz  (internal oscillator; BOD disabled), Platform=avr, Package=attiny 1.5.8-2
+	Hardware: ATtiny x5 w/ ATtiny85 @ 8 MHz  (internal oscillator; BOD disabled), Platform=avr, Package=attiny 1.5.8-2
 */
 
 #define __AVR_ATtiny85__
 #define __AVR_ATTINY85__
 #define ARDUINO 160
 #define ARDUINO_MAIN
-#define F_CPU 1000000L
+#define F_CPU 8000000L
 #define __AVR__
 extern "C" void __cxa_pure_virtual() {;}
 
 //
 //
-//
 void sleep();
 void beep();
+void check_battery();
+void setup_watchdog(int timerPrescaler);
 void battStatus();
+void wakeup();
+void send_data(int data);
+void LED_on();
+void LED_off();
 
 #include "C:\Users\Tahsin\Documents\Arduino\hardware\attiny 1.5.8-2\avr\cores\tiny\arduino.h"
 #include <TX_Sensor.ino>
