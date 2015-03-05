f53ba3fb31eb498215adda2dfb0abf6d75ff6aa4
Tahsin Rahman <madhacker3kxl@hotmail.com>
2015-03-05 15:28:44 -0600

    Change pin read for wdt logic. Change led status.

--- TX_Sensor/TX_Sensor.ino
+++ TX_Sensor/TX_Sensor.ino
@@ -6,12 +6,12 @@
 #include <analogComp-master/analogComp.h>
 
 #define TX   PINB4      //TX Pin
-#define LED  PINB0		//LED Pin
+#define LED  PINB3		//LED Pin
 #define Sens PINB2      //Input pin
 
-#define low_Batt_code    101010 //Code for low battery
-#define Open_D           8767848
-#define Close_D          8767847
+#define low_Batt_code    13976880 //Code for low battery
+const int Open_D =           8767848;
+const int Close_D =         8767847;
 
 RCSwitch mySwitch = RCSwitch();
 
@@ -30,44 +30,48 @@ void setup() {
     power_timer0_enable();  //Turn on timer0 clock on
     power_timer1_enable();  //Turn on timer1 clock on
 
+    //Setup needs pins
     DDRB  |= (1 << LED);	//Set notification pin as output
     PORTB &= ~(1 << LED);   //Set the notification pin as low just in case
 
-    DDRB  &= ~(1<< Sens);    //Set Sens pin as input
+    DDRB  &= ~(1<< Sens);   //Set Sens pin as input
+    PORTB &= ~(1 << Sens);  //Tri-state the input (no pull-up)
 
-    mySwitch.enableTransmit(TX);    //Enable transmit on pin TX
-    mySwitch.setRepeatTransmit(2);  //Send the code twice just in case
+    DDRB  |= (1 << TX);	    //Set notification pin as output
 
-    attachInterrupt(INT0, wakeup, LOW);
+    mySwitch.enableTransmit(TX);     //Enable transmit on pin TX
+
+    attachInterrupt(0, wakeup, LOW); // Setup initial interrupt from sleep
 
     //Notify the user mcu has loaded fine
-    beep();
     beep();
 }
 
 void loop() {
-    if (wdt_setup) {
+
+    if ((1<<Sens))
+    {
+        wdt_setup = false;          //Turn off WDT setup function
+        wdt_disable();              //Turn off the WDT!
+        //enable_batt_check();        //Enable comparitor for battery check
+
+        send_data(Close_D);         //Send data for closed contact
+        //check_battery();
+        attachInterrupt(0, wakeup, LOW);   //Turn on wake up interrupt
+    }
+
+    if (wdt_setup)
+    {
         //enable_batt_check();
         send_data(Open_D); //Send data out for open contact
         //check_battery();
         setup_watchdog(8); //Turn on watchdog for 4 sec delay
-        wdt_setup = false;
-    }
-
-    if (Sens)
-    {
-        wdt_disable();                        //Turn off the WDT!
-        //enable_batt_check();
-
-        send_data(Close_D);                   //Send data for closed contact
-        //check_battery();
-
-        attachInterrupt(INT0, wakeup, LOW);   //Turn on wake up interrupt
+        beep();
+        beep();
     }
 
     sleep();
 }
-
 
 void sleep(){
     set_sleep_mode(SLEEP_MODE_PWR_DOWN); //Set sleep mode to deep sleep
@@ -79,14 +83,14 @@ void sleep(){
     sleep_disable();                     //Disable sleep
 }
 
-void beep(){
+void beep(){ //For various status display function
     LED_on();
     delay(500);
     LED_off();
     delay(500);
 }
 
-void enable_batt_check(){
+void enable_batt_check(){ //Functions for analog setup
      analogComparator.setOn(INTERNAL_REFERENCE, AIN1);  //Setup analog comparator
      analogComparator.enableInterrupt(battStatus, LOW); //Set interrupt for comparator
 }
@@ -105,9 +109,9 @@ void setup_watchdog(int timerPrescaler) {
 
     //This order of commands is important and cannot be combined
     MCUSR &= ~(1<<WDRF);                         //Clear the watch dog reset
-//    WDTCR |= (1<<WDCE) | (1<<WDE);               //Set WD_change enable, set WD enable
-//    WDTCR = bb;                                  //Set new watchdog timeout value
- //   WDTCR |= _BV(WDIE);                          //Set the interrupt enable, this will keep unit from resetting after each int
+    WDTCR |= (1<<WDCE) | (1<<WDE);               //Set WD_change enable, set WD enable
+    WDTCR  = bb;                                 //Set new watchdog timeout value
+    WDTCR |= _BV(WDIE);                          //Set the interrupt enable, this will keep unit from resetting after each int
 }
 
 //interrupt to be raised by the analog comparator
@@ -115,10 +119,13 @@ void battStatus(){
     lowBattery = true; //let's inform the main loop that the condition has been reached by the analog comparator
 }
 
-//interrupt for contact open
-void wakeup(){
-        wdt_setup = true;
-        detachInterrupt(INT0);
+void wakeup(){  //interrupt for contact open
+        detachInterrupt(0);     //Turn off pin interrupt
+        wdt_setup = true;       //Activate the watch-dog setup function
+
+        //Set up the pins again just incase
+        DDRB  &= ~(1<< Sens);   //Set Sens pin as input
+        PORTB &= ~(1 << Sens);  //Tri-state the input (no pull-up)
 }
 
 void send_data(int data){
@@ -133,4 +140,8 @@ void LED_on(){
 
 void LED_off(){
     PORTB &= ~(1 << LED);    //Turn off LED
+}
+
+ISR(WDT_vect) { //Watch-dog interrupt vector
+    //Don't do anything. This is just here so that we wake up.
 }
